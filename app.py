035d962708fd07bb2d316e4fd8c0a974eb6d26c4
Kalliana <kalliana.glaeser@gmail.com>
2026-02-08 20:24:01 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,61 @@
+import os
+from uuid import main
+
+def exibir_nome_do_programa():
+
+    print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+""")
+
+def exibir_menu():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Saindo do sistema\n')
+
+def opcao_invalida():
+    print("Opção inválida. Tente novamente.\n")
+    input("Pressione ENTER para continuar...")
+    main()
+
+def cadastrar_restaurante():
+    print("Cadastrar restaurante")
+
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        print(f'Você escolheu a opção {opcao_escolhida}')
+
+        if opcao_escolhida == 1:
+            print("Cadastrar restaurante")
+        elif opcao_escolhida == 2:
+            print("Listar restaurante")
+        elif opcao_escolhida == 3:
+            print("Ativar restaurante")
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+
+
+def main():
+    os.system('cls')
+    exibir_nome_do_programa()
+    exibir_menu()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
