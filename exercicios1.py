a358cebdccf6e2841a411014a6467bd71459bc73
Kalliana <kalliana.glaeser@gmail.com>
2026-02-09 21:10:22 -0300

    Create exercicios1.py

---
+++ exercicios1.py
@@ -0,0 +1,24 @@
+#1-Imprima a frase: Python na Escola de Programação da Alura.
+print('Python na Escola de Programação da Alura\n')
+
+print("********************************************************")
+
+#2- Imprima a frase: Meu nome é {nome} e tenho {idade} anos
+nome = 'Kalliana'
+idade = 30
+# Abordagem do f-string
+print(f'Meu nome é {nome} e tenho {idade} anos.\n')
+
+print("********************************************************")
+
+#3- Imprima a palavra: ‘ALURA’ de modo que cada letra fique em uma linha
+print('A','L','U','R','A',sep='\n')
+
+print("********************************************************")
+
+#4 - Para imprimir o valor de pi arredondado
+pi = 3.14159
+# Abordagem de .format()
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
+
+
