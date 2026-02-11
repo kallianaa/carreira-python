2824c3778086a0da654c50f99090c5e44a202a21
Kalliana <kalliana.glaeser@gmail.com>
2026-02-11 18:44:57 -0300

    Create exercicios3.py

---
+++ exercicios3.py
@@ -0,0 +1,22 @@
+
+#1 - Crie uma lista para cada informação a seguir:
+# Lista de números de 1 a 10;
+# Lista com quatro nomes;
+# Lista com o ano que você nasceu e o ano atual.
+
+
+
+# 2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
+
+# 3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10
+
+# 4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
+
+# 5 - Solicite ao usuário um número e, em seguida, utilize um loop for para imprimir a tabuada desse número,
+# indo de 1 a 10.
+
+# 6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos.
+# Utilize um bloco try-except para lidar com possíveis exceções.
+
+# 7 - Construa um código que calcule a média dos valores em uma lista.
+# Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
