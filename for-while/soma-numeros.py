8d4cbec237cf473a0334cc35bf7878d145add7c9
Kalliana <kalliana.glaeser@gmail.com>
2026-02-16 09:25:29 -0300

    aprendendo for-while

---
+++ for-while/soma-numeros.py
@@ -0,0 +1,11 @@
+valores = [10, 20, 30, 40, 50]
+soma = sum(valores)
+print(f"A soma total das receitas é: {soma}")
+
+
+numeros = [10, 20, 30, 40, 50]
+soma = 0
+for numero in numeros:
+    soma += numero
+
+print(f"A soma total das receitas é: {soma}")
