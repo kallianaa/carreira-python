8edbf43413aa2a8942d10f04416fc288bfc04b0c
kall!ana <kalliana.glaeser@gmail.com>
2026-04-04 15:18:04 -0300

    Create main.py

---
+++ orientacao-objetos/books/main.py
@@ -0,0 +1,7 @@
+from Livros import Livro
+
+livro0 = Livro("Destrua este diário", "Keri Smith", 2007)
+livro1 = Livro("Queime Depois De Escrever", "Keri Smith", 2013)
+
+print(livro0)
+print(livro1)
