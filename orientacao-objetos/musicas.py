24f87ab86ed3f94792392bd20f4592978cf6adaf
kall!ana <kalliana.glaeser@gmail.com>
2026-04-04 17:43:20 -0300

    Create musicas.py

---
+++ orientacao-objetos/musicas.py
@@ -0,0 +1,21 @@
+class Musica:
+    nome = ""
+    artista = ""
+    duracao = int
+
+musica1 = Musica()
+musica1.nome = 'Bohemian Rhapsody'
+musica1.artista = 'Queen'
+musica1.duracao = 355
+
+print(f'Música: {musica1.nome} - Banda: {musica1.artista} - {musica1.duracao} segundos')
+
+musica2 = Musica()
+musica2.nome = 'Imagine'
+musica2.artista = 'John Lennon'
+musica2.duracao = 183
+
+musica3 = Musica()
+musica3.nome = 'Shape of You'
+musica3.artista = 'Ed Sheeran'
+musica3.duracao = 234
